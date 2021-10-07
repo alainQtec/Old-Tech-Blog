6f704568d09bdbcc6b6b61741d70aee7de1dd6bf
Alain Herve <alain.1337dev@outlook.com>
2021-10-07 06:39:40 -0700

    Update Blog.html, tags.html ...

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -229,37 +229,3 @@ function scrollFooter(scrollY, heightFooter) {
     });
   }
 }
-
-$(window).load(function () {
-  var windowHeight = $(window).height(),
-    footerHeight = $('footer').height(),
-    heightDocument = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
-
-  $('#scroll-animate, #scroll-animate-main').css({
-    'height': heightDocument + 'px'
-  });
-  $('header').css({
-    'height': windowHeight + 'px',
-    'line-height': windowHeight + 'px'
-  });
-
-  $('.wrapper-parallax').css({
-    'margin-top': windowHeight + 'px'
-  });
-
-  scrollFooter(window.scrollY, footerHeight);
-
-  window.onscroll = function () {
-    var scroll = window.scrollY;
-
-    $('#scroll-animate-main').css({
-      'top': '-' + scroll + 'px'
-    });
-
-    $('header').css({
-      'background-position-y': 50 - (scroll * 100 / heightDocument) + '%'
-    });
-
-    scrollFooter(scroll, footerHeight);
-  }
-});
