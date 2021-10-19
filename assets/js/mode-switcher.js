ae79203aad7f546ad60b0e966ea375cbc9136819
Alain Herve <alain.1337dev@outlook.com>
2021-10-18 23:19:11 -0700

    update header img

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -19,20 +19,24 @@ function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
+		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'light');
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
+		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {
 		document.documentElement.setAttribute('data-theme', 'light');
+		document.getElementById("recent-articles-header").setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
 		//let theme = sessionStorage.getItem('theme');
 		//console.log("this was triggered");
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
+		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
@@ -42,10 +46,12 @@ if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
 	document.getElementById("theme-toggle").checked = false;
+	document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 	// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
 	document.getElementById("theme-toggle").checked = true;
+	document.getElementById("recent-articles-header").setAttribute('data-theme', 'light');
 	// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
