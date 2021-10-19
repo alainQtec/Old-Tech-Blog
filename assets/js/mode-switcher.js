ad278a098c2345e0871d957754bd5e0657e09c76
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-19 19:28:08 +0000

    Deploy alainQtec/tech-blog to github.com/alainQtec/tech-blog.git:gh-pages

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -3,11 +3,11 @@ let theme = sessionStorage.getItem('theme');
 
 function prefersColorTest(systemInitiatedDark) {
 	if (systemInitiatedDark.matches) {
-		document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-theme', 'light');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 		sessionStorage.setItem('theme', '');
 	} else {
-		document.documentElement.setAttribute('data-theme', 'light');
+		document.documentElement.setAttribute('data-theme', 'dark');
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 		sessionStorage.setItem('theme', '');
 	}
@@ -19,26 +19,20 @@ function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
-		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
+		document.getElementById("recent-articles-header").setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {
 		document.documentElement.setAttribute('data-theme', 'light');
 		document.getElementById("recent-articles-header").setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		//let theme = sessionStorage.getItem('theme');
-		//console.log("this was triggered");
-		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }
 
@@ -47,11 +41,9 @@ if (theme === "dark") {
 	sessionStorage.setItem('theme', 'dark');
 	document.getElementById("theme-toggle").checked = false;
 	document.getElementById("recent-articles-header").setAttribute('data-theme', 'dark');
-	// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
 	document.getElementById("theme-toggle").checked = true;
 	document.getElementById("recent-articles-header").setAttribute('data-theme', 'light');
-	// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
