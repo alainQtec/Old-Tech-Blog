ca2d005be1bf338961222b918a3c225d166b0526
Alain Herve <alain.1337dev@outlook.com>
2021-11-18 05:15:57 -0800

    update pa modal

--- _includes/scripts/modal.js
+++ _includes/scripts/modal.js
@@ -1,15 +1,1 @@
-$(document).ready(function () {
-  $(".modal-cancel").on("click", function () {
-    if ($('#useragreemInput').val() == 'true') {
-      $('.modal-close').trigger('click');
-    } else {
-      alert('pls click on checkbox');
-    }
-  });
-  $('.one').change(function () {
-    if (this.checked) {
-      $(this).prop("checked", 'true');
-    }
-    $('#useragreemInput').val(this.checked);
-  });
-});
+// nothing to see here !
